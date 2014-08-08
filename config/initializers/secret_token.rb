c4897d49849c29747d0671088c17d0e970c1d361
donaminos <amine.sadry@gmail.com>
2014-08-08 15:21:07 +0200

    Initial repo commit with ignore file and Gemfile updated for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SdApp::Application.config.secret_key_base = 'a60d843de695c34cab64cc6df8320af650e87b88203311f29ca37b908f36a28ea0686c46ff57d558ce518232bcd0f31a9e5af6f9852aeb167d368da46fdac640'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SdApp::Application.config.session_store :cookie_store, key: '_sd-app_session'
