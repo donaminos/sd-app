256b347d186481eba2dd824a235331deb5d5430e
amine sadry <amine.sadry@gmail.com>
2015-03-20 11:05:32 +0100

    fix course friendly_id

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,17 +4,21 @@ class Course < ActiveRecord::Base
   belongs_to :level
   
   extend FriendlyId
-  friendly_id :slug_candidates, use: :slugged
+  friendly_id :slug_candidates, use: [:slugged, :history]
 
   
   def slug_candidates
     [
-      :title,
-      [:title, :created_at]
+      :title
+      #[:title, :created_at]
       #[:title, :description],
-      #[:title, :description, :created_at]
-      
+      #[:title, :description, :created_at]   
     ]
+  end
+
+  def should_generate_new_friendly_id
+    title_chaged? || !self.slug.present?
+    
   end
 
 end
